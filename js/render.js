bfd2aa60fa71489df444f255bded2d8de29e220d
Inumidun Amao <seindelawson@yahoo.com>
2017-01-13 21:00:53 +0100

    Adding user interface layout. Ready for styling. Merging search and book function with it

--- js/render.js
+++
@@ -1,8 +0,0 @@
-function rToDiv(d) {
-	var json_data = JSON.parse(d);
-	console.log(json_data[0].booked);
-	document.getElementById("test").innerHTML = json_data;
-	//return json_data[0].booked;
-}
-
-rToDiv(data);
